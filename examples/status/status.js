4dd15953fb2ff48301692696205126722a2c7102
Muhammad Abdullah <abdullahquddus7@gmail.com>
2023-10-26 16:47:32 +0500

    first push

--- examples/status/status.js
+++ examples/status/status.js
@@ -1,12 +1,14 @@
-#!/usr/bin/env node
+var esl = require('modesl');
+const playRecording = require('./playRecording');
 
-var esl = require('modesl'),
 //open a connection
 conn = new esl.Connection('127.0.0.1', 8021, 'ClueCon', function() {
     //send the status api command
     conn.api('status', function(res) {
         //log result body and exit
         console.log(res.getBody());
+        playRecording.ivrMenu(conn);
         process.exit(0);
     });
+
 });
