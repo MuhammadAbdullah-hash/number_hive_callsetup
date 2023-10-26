9322f546a5acb596b970cc610b9dadbb367572c4
Muhammad Abdullah <abdullahquddus7@gmail.com>
2023-10-26 16:55:20 +0500

    push-2

--- examples/status/status.js
+++ examples/status/status.js
@@ -2,7 +2,7 @@ var esl = require('modesl');
 const playRecording = require('./playRecording');
 
 //open a connection
-conn = new esl.Connection('127.0.0.1', 8021, 'ClueCon', function() {
+conn = new esl.Connection('52.25.153.34', 8021, 'ClueCon', function() {
     //send the status api command
     conn.api('status', function(res) {
         //log result body and exit
